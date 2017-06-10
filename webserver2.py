b2dd0486c6760c92b38a6e36cbf1b879fbdb4e20
tendaitt <tendaitt@gmail.com>
2017-06-10 01:23:04 -0400

    re-copied file, switched from tabs to spaces

--- webserver2.py
+++ webserver2.py
@@ -6,80 +6,80 @@ import sys
 
 class WSGIServer(object):
 
-	address_family = socket.AF_INET
-	socket_type = socket.SOCK_STREAM
-	request_queue_size = 1
+    address_family = socket.AF_INET
+    socket_type = socket.SOCK_STREAM
+    request_queue_size = 1
 
-	def _init_(self, server_address):
-		# Create a listening socket
-		self.listen_socket = listen_socket = socket.socket(
-			self.address_family,
-			self.socket_type
-		)
-		# Allow to reuse same address
-		listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		# Bind
-		listen_socket.bind(server_address)
-		# Activate
-		listen_socket.listen(self.request_queue_size)
-		# Get server host name and port
-		host, port = self.listen_socket.getsockname()[:2]
-		self.server_name = socket.getfqdn(host)
-		self.server_port = port
-		# Return headers set by Web framework/Web application
-		self.headers_set = []
-
-
-	def set_app(self, application):
-		self.application = application
+    def _init_(self, server_address):
+        # Create a listening socket
+        self.listen_socket = listen_socket = socket.socket(
+            self.address_family,
+            self.socket_type
+        )
+        # Allow to reuse same address
+        listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # Bind
+        listen_socket.bind(server_address)
+        # Activate
+        listen_socket.listen(self.request_queue_size)
+        # Get server host name and port
+        host, port = self.listen_socket.getsockname()[:2]
+        self.server_name = socket.getfqdn(host)
+        self.server_port = port
+        # Return headers set by Web framework/Web application
+        self.headers_set = []
 
 
-	def serve_forever(self):
-		listen_socket = self.listen_socket
-		while True:
-			# New client connection
-			self.client_connection, client_address = listen_socket.accept()
-			# Handle one request and close the client connection. Then loop
-			# over to wait for another client connection
-			self.handle_one_request()
+    def set_app(self, application):
+        self.application = application
 
-	def handle_one_request(self):
-		self.request_data = request_data = self.client_connection.recv(1024)
-		# Print formatted request data a la 'curl -v'
-		print(''.join(
-			'< {line}\n'.format(line=line)
-			for line in request_data.splitlines()
-		))
 
-		self.parse_request(request_data)
+    def serve_forever(self):
+        listen_socket = self.listen_socket
+        while True:
+            # New client connection
+            self.client_connection, client_address = listen_socket.accept()
+            # Handle one request and close the client connection. Then loop
+            # over to wait for another client connection
+            self.handle_one_request()
 
-		# Construct environment dictionary using request data
-		env = self.get_environ()
+    def handle_one_request(self):
+        self.request_data = request_data = self.client_connection.recv(1024)
+        # Print formatted request data a la 'curl -v'
+        print(''.join(
+            '< {line}\n'.format(line=line)
+            for line in request_data.splitlines()
+        ))
 
-		# It's time to call our application callable and get back a
-		# result that will become the HTTP response body
-		result = self.application(env, self.start_response)
+        self.parse_request(request_data)
 
-		#Construct a response and send it back to thee client
-		self.finish_response(result)
+        # Construct environment dictionary using request data
+        env = self.get_environ()
 
-	def parse_request(self, text):
-		request_line = text.splitlines()[0]
-		request_line = request_line.rstrip('\r\n')
-		# Break down the request line into components
-		(self.request_method, #GET
-		 self.path,           # /hello
-		 self.request_version # HTTP/1.1
-		 ) = request_line.split()
+        # It's time to call our application callable and get back a
+        # result that will become the HTTP response body
+        result = self.application(env, self.start_response)
 
-	def get_environ(self):
-		env = {}
-		# The following code snippet does not follow PEP8 conventions
-		# but it's formatted the way it is for demonstration purposes to
-		# emphasize the required variables and their values
-		#
-		# Required WSGI variables
-		env['wsgi.version']      = (1, 0)
+        #Construct a response and send it back to thee client
+        self.finish_response(result)
+
+    def parse_request(self, text):
+        request_line = text.splitlines()[0]
+        request_line = request_line.rstrip('\r\n')
+        # Break down the request line into components
+        (self.request_method,  # GET
+         self.path,            # /hello
+         self.request_version  # HTTP/1.1
+         ) = request_line.split()
+
+    def get_environ(self):
+        env = {}
+        # The following code snippet does not follow PEP8 conventions
+        # but it's formatted the way it is for demonstration purposes
+        # to emphasize the required variables and their values
+        #
+        # Required WSGI variables
+        env['wsgi.version']      = (1, 0)
         env['wsgi.url_scheme']   = 'http'
         env['wsgi.input']        = StringIO.StringIO(self.request_data)
         env['wsgi.errors']       = sys.stderr
@@ -128,14 +128,14 @@ class WSGIServer(object):
 
 
     def make_server(server_address, application):
-    	server = WSGIServer(server_address)
-    	server.set_app(application)
-    	return server
+        server = WSGIServer(server_address)
+        server.set_app(application)
+        return server
 
 
     if __name__ == '__main__':
-    	if len(sys.argv) < 2:
-    		sys.exit('Provide a WSGI application object as module:callable')
+        if len(sys.argv) < 2:
+            sys.exit('Provide a WSGI application object as module:callable')
         app_path = sys.argv[1]
         module, application = app_path.split(':')
         module = __import__(module)
